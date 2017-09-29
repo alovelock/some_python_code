84d672201059d069e634c33602f3bacb0566e365
Aaron Lovelock <aaronlovelock4@gmail.com>
2017-09-29 17:31:25 +1000

    Machine Learning stocks.

--- Getting_SP_500.py
+++ Getting_SP_500.py
@@ -1,7 +1,11 @@
 import bs4 as bs
 import pickle
 import requests
-
+import datetime as dt
+import os
+import pandas as pd
+import pandas_datareader as web
+import time
 
 def save_sp500_tickers():
     resp = requests.get('http://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
@@ -17,5 +21,33 @@ def save_sp500_tickers():
 
     return tickers
 
+#save_sp500_tickers()
 
-save_sp500_tickers()
+def get_data_from_yahoo(reload_sp500=False):
+
+    if reload_sp500:
+        tickers = save_sp500_tickers()
+    else:
+        with open("sp500tickers.pickle", "rb") as f:
+            tickers = pickle.load(f)
+
+    if not os.path.exists("stock_dfs"):
+        os.makedirs("stock_dfs")
+
+    start = dt.datetime(2000, 1, 1)
+    end = dt.datetime(2016, 12, 31)
+
+    for ticker in tickers:
+
+        if not os.path.exists("stock_dfs/{}.csv".format(ticker)):
+            df = web.DataReader(ticker, "yahoo", start, end)
+            df.to_csv("stock_dfs/{}.csv".format(ticker))
+            print("Got ticker {}".format(ticker))
+            time.sleep(2)
+        else:
+            print("Already have {}".format(ticker))
+
+
+
+get_data_from_yahoo()
+
