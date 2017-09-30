4efeb8135b78d962bbde193ad428d44bf4662c01
Aaron Lovelock <aaronlovelock4@gmail.com>
2017-09-30 12:09:29 +1000

    Update working code

--- Getting_SP_500_2.py
+++ Getting_SP_500_2.py
@@ -173,7 +173,10 @@ def do_ml(ticker):
     X, y, df = extract_featuresets(ticker)
 
     X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.25)
-    clf = neighbors.KNeighborsClassifier()
+    #clf = neighbors.KNeighborsClassifier()
+    clf = VotingClassifier([('lsvc',svm.LinearSVC()),
+                            ('knn',neighbors.KNeighborsClassifier()),
+                            ('rfor',RandomForestClassifier())])
     clf.fit(X_train, y_train)
 
     confidence = clf.score(X_test, y_test)
@@ -186,3 +189,4 @@ def do_ml(ticker):
 
 do_ml("XOM")
 do_ml("AAPL")
+do_ml("BA")
